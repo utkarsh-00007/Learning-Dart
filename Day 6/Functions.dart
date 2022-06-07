6ab1ce4c4df1403efd96fd51260d38a02051658c
utkarsh-00007 <dwivediutkarsh525@gmail.com>
2022-06-08 00:54:24 +0530

    Day 6 learning functions in dart

---
+++ Day 6/Functions.dart
@@ -0,0 +1,18 @@
+/*
+Types of functions : 
+1- System Defined Functions : main(),print(),write()
+2- User Defined / custom Functions 
+
+Functions have Two Parts :
+1- Function Definition 
+what are tasks erformed by function
+preferred outside of main 
+2- Function Calling 
+*/
+void main(List<String> args) {
+  printsomething();
+}
+
+printsomething() {
+  print("hello");
+}
